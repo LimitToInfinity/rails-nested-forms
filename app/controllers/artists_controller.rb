369cceced0ade7216136bb7aa6216ab41aa73046
github username <github email address>
2019-07-05 16:12:50 -0600

    Nested 3 paintings forms into Artist new form.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,10 +1,17 @@
 class ArtistsController < ApplicationController
+  
+  before_action :find_artist, only: [:show]
+
   def index
     @artists = Artist.all
   end
 
+  def show
+  end
+
   def new
     @artist = Artist.new
+    3.times { @artist.paintings.build }
   end
 
   def create
@@ -18,8 +25,14 @@ class ArtistsController < ApplicationController
   end
 
   private
+  def find_artist
+    @artist = Artist.find(params[:id])
+  end
 
   def artist_params
-    params.require(:artist).permit(:name)
+    params.require(:artist).permit(
+      :name,
+      paintings_attributes: [:title]
+      )
   end
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,5 @@
 class Artist < ApplicationRecord
   has_many :paintings
+
+  accepts_nested_attributes_for :paintings, reject_if: :all_blank
 end
